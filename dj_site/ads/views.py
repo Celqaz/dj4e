7f6d07c1d958b5f10f575658f0963c5367d4a30b
“celqaz” <819248392@qq.com>
2021-02-28 15:57:00 +0800

    4th week of "Using JavaScript, JQuery, and JSON in Django"

--- dj_site/ads/views.py
+++ dj_site/ads/views.py
@@ -6,6 +6,8 @@ from django.views.generic.base import View
 from .owner import *
 from .models import Ad, Comment, Fav
 from .forms import CreateForm, CommentForm
+from django.db.models import Q
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 # Create your views here.
@@ -14,6 +16,7 @@ class AdListView(OwnerListView):
     template_name = 'ads/index.html'
 
     def get(self, request):
+        # fav star
         ad_list = Ad.objects.all()
         favorites = list()
         if request.user.is_authenticated:
@@ -21,6 +24,25 @@ class AdListView(OwnerListView):
             rows = request.user.favorite_ads.values('id')
             # favorites = [2, 4, ...] using list comprehension
             favorites = [row['id'] for row in rows]
+
+        # search
+        strval = request.GET.get("search", False)
+        if strval:
+            # Simple title-only search
+            # objects = Post.objects.filter(title__contains=strval).select_related().order_by('-updated_at')[:10]
+
+            # Multi-field search
+            # __icontains for case-insensitive search
+            query = Q(title__icontains=strval)
+            query.add(Q(text__icontains=strval), Q.OR)
+            objects = Ad.objects.filter(query).select_related().order_by('-updated_at')[:10]
+        else:
+            objects = Ad.objects.all().order_by('-updated_at')[:10]
+
+        # Augment the post_list
+        for obj in objects:
+            obj.natural_updated = naturaltime(obj.updated_at)
+
         ctx = {'ad_list': ad_list, 'favorites': favorites}
         return render(request, self.template_name, ctx)
 
